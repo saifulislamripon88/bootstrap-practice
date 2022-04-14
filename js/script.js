1cd3c15e03d0d03d03386676c6674d2793f7e971
saifulislamripon <si3491061@gmail.com>
2022-04-15 03:30:50 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//sum
+
+// const add = (a, b) => a + b;
+// const a=5
+// document.getElementById("a").value;
+// const b= 6
+// document.getElementById("b").value;
+// const modulus = (a, b) => a % b;
+
+// let result = add(a, b);
+// document.getElementById("result").innerHTML = result;
+
+// let result = subtract(a, b);
+// document.getElementById("result").innerHTML = result;
+
+
+
+
+// const subtract = (a, b) => a - b;
+// const multiply = (a, b) => a * b;
+// const divide = (a, b) => a / b;
+
+// sum two numbers
+
